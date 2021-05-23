d2f26178f689ead56493dd1642b6373476e611fb
Andrew Ivanov <andrew.starwind@gmail.com>
2021-05-23 09:01:57 +0300

    Fixed OGRN

--- src/utils/generateOgrn.js
+++ src/utils/generateOgrn.js
@@ -3,9 +3,16 @@ import { generateValue } from './helpers'
 const IE_LENGTH = 15
 const ORG_LENGTH = 13
 
+const LEGAL_ENTITY_ID_1 = 1
+const LEGAL_ENTITY_ID_2 = 5
+const IE_ID = 3
+const NUMBER_ID = 2
+const allowsFirstDig = [LEGAL_ENTITY_ID_1, IE_ID, NUMBER_ID, LEGAL_ENTITY_ID_2]
+
 const generate = (isIE) => {
-  const value = generateValue((isIE ? IE_LENGTH : ORG_LENGTH) - 1)
-  const ctrlSum = parseInt(value, 10) % (value.length - 1) % 10
+  const firstDigit = allowsFirstDig[Math.round(Math.random() * 10) % allowsFirstDig.length]
+  const value = firstDigit.toString() + generateValue((isIE ? IE_LENGTH : ORG_LENGTH) - 2)
+  const ctrlSum = (parseInt(value, 10) % (value.length - 1)) % 10
 
   return value + ctrlSum
 }
