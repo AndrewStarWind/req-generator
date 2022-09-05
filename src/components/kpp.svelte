3db4a0bd25bfc530b55a3ce056aa59e154e75934
au.sommer <au.sommer@tensor.ru>
2022-09-05 13:03:09 +0300

    Fixes for manifest v3 and some redesign feature.

--- src/components/kpp.svelte
+++ src/components/kpp.svelte
@@ -12,25 +12,31 @@
       { id: KPP_TYPES.registration, text: "По месту регистрации" },
       { id: KPP_TYPES.bigTaxPayer, text: "Крупнейший налогоплательщик" },
       { id: KPP_TYPES.envd, text: "ЕНВД" },
-      { id: KPP_TYPES.branch, text: "кпп филиала" },
+      { id: KPP_TYPES.branch, text: "КПП филиала" },
    ];
    let type = "01";
    let value = Store.getValue("KPP");
 
-   export const generate = () => value = generateKPP(type)
+   export const generate = () => (value = generateKPP(type));
    const generateAndCopy = () => {
-      const generatedValue = generate()
+      const generatedValue = generate();
 
-      copyTextToClipboard(generatedValue)
-      return generatedValue
-   }
+      copyTextToClipboard(generatedValue);
+      return generatedValue;
+   };
 </script>
 
 <div class="form-group">
    <Textfield type="text" bind:value label="КПП:" />
    <Select bind:value={type} label="ТИП/КФХ:" class="select">
       {#each types as item}
-         <Option value={item.id}>{item.text}</Option>
+         <Option value={item.id} title={item.text}>
+            <div class="select__item">
+               <span>
+                  {item.text}
+               </span>
+            </div>
+         </Option>
       {/each}
    </Select>
    <div class="form-group__actions">
